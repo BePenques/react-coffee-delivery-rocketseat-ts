6b76f9d042d1024b740b2a5111b9bbd375a690b6
Betiza Barreira <be.penques@hotmail.com>
2024-06-26 11:48:38 -0300

    rotas e defaultlayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <h1>hello baby</h1>
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home/index'
+import { Checkout } from './pages/Checkout/index'
+import { Success } from './pages/Success/index'
+import { DefaultLayout } from './layouts/DefaultLayout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
+
+      {/* <Route path="/admin" element={<AdminLayout/>}>
+        <Route path="/products"/>
+        </Route> */}
+      {/* http://localhost:5173/admin/products */}
+    </Routes>
+  )
+}
