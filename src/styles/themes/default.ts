987bb34ceb5220d391bbd13eaa341c395a46def0
Betiza Barreira <be.penques@hotmail.com>
2024-06-26 10:49:33 -0300

    arquivos e configurações iniciais

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+    white: '#fff',
+
+    'yellow-700': '#C47F17',
+    'yellow-400': '#DBAC2C',
+    'yellow-100': '#F1E9C9',
+
+    'purple-700': '#4B2995',
+    'purple-400': '#8047F8',
+    'purple-100': '#EBE5F9',
+
+    'gray-700': '#272221',
+    'gray-600': '#403937',
+    'gray-400': '#574F4D',
+    'gray-300': '#8D8686',
+    'gray-100': '#D7D5D5',
+
+    'gray-50': '#E6E5E5',
+    'gray-40': '#EDEDED',
+    'gray-30': '#F3F2F2',
+    'gray-10': '#FAFAFA',
+
+}
